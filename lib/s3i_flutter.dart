475057c5d2bd12388af2b5221842372bffa60a82
LukasPoque <lukas.poque@rwth-aachen.de>
2021-07-06 14:27:54 +0200

    Add export statements for package

--- lib/s3i_flutter.dart
+++ lib/s3i_flutter.dart
@@ -1,1 +1,14 @@
 library s3i_flutter;
+export 'src/s3i_core.dart';
+//directory
+export 'src/directory/thing.dart';
+export 'src/directory/dir_object.dart';
+export 'src/directory/endpoint.dart';
+export 'src/directory/link.dart';
+export 'src/directory/location.dart';
+export 'src/directory/value.dart';
+//exceptions
+export 'src/exceptions/s3i_exception.dart';
+export 'src/exceptions/invalid_json_schema_exception.dart';
+export 'src/exceptions/json_missing_key_exception.dart';
+export 'src/exceptions/parse_exception.dart';
