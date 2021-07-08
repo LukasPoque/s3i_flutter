034931e1328d8ed11a2a6e245dbe5ca7099fa2c0
LukasPoque <lukas.poque@rwth-aachen.de>
2021-07-08 16:15:26 +0200

    update lib export statements

--- lib/s3i_flutter.dart
+++ lib/s3i_flutter.dart
@@ -1,5 +1,7 @@
 library s3i_flutter;
+
 export 'src/s3i_core.dart';
+
 //directory
 export 'src/directory/thing.dart';
 export 'src/directory/dir_object.dart';
@@ -7,14 +9,24 @@ export 'src/directory/endpoint.dart';
 export 'src/directory/link.dart';
 export 'src/directory/location.dart';
 export 'src/directory/value.dart';
+
 //exceptions
 export 'src/exceptions/s3i_exception.dart';
 export 'src/exceptions/invalid_json_schema_exception.dart';
 export 'src/exceptions/json_missing_key_exception.dart';
 export 'src/exceptions/parse_exception.dart';
+export 'src/exceptions/max_retry_exception.dart';
+export 'src/exceptions/network_response_exception.dart';
+
 //query
 export 'src/query/query_assembler.dart';
 export 'src/query/field_query.dart';
 export 'src/query/namespace_query.dart';
 export 'src/query/option_query.dart';
 export 'src/query/rql_query.dart';
+
+//auth
+export 'src/auth/tokens.dart';
+export 'src/auth/client_identity.dart';
+export 'src/auth/authentication_manager.dart';
+export 'src/auth/oauth_proxy_flow.dart';
