c42dc9166db64419da5ddea57043ae0b0b9f2ef3
LukasPoque <lukas.poque@rwth-aachen.de>
2021-08-05 22:16:54 +0200

    Improve json_key

--- lib/src/utils/json_key.dart
+++ lib/src/utils/json_key.dart
@@ -2,126 +2,125 @@
 class JsonKey {
   // TODO(poq): add documentation
 // ditto
-  ///
+  ///thingId
   static const String thingId = 'thingId';
 
-  ///
+  ///policyId
   static const String policyId = 'policyId';
 
-  ///
+  ///attributes
   static const String attributes = 'attributes';
 
-  ///
+  ///items
   static const String items = 'items';
 
-  ///
+  ///cursor
   static const String cursor = 'cursor';
 
-  ///
+  ///type
   static const String type = 'type';
 
-  ///
+  ///expiry
   static const String expiry = 'expiry';
 
-  ///
+  ///grant
   static const String grant = 'grant';
 
-  ///
+  ///revoke
   static const String revoke = 'revoke';
 
-  ///
+  ///subjects
   static const String subjects = 'subjects';
 
-  ///
+  ///resources
   static const String resources = 'resources';
 
-  ///
+  ///entries
   static const String entries = 'entries';
 
 // keycloak
-  ///
+  ///grant_type
   static const String grantType = 'grant_type';
 
-  ///
-  ///
+  ///client_id
   static const String clientId = 'client_id';
 
-  ///
+  ///client_secret
   static const String clientSecret = 'client_secret';
 
-  ///
+  ///access_token
   static const String accessToken = 'access_token';
 
-  ///
+  ///refresh_token
   static const String refreshToken = 'refresh_token';
 
 // (f)ml40 specific keys
-  ///
+  ///name
   static const String name = 'name';
 
-  ///
+  ///type
   static const String thingType = 'type';
 
-  ///
+  ///dataModel
   static const String dataModel = 'dataModel';
 
-  ///
+  ///publicKey
   static const String publicKey = 'publicKey';
 
-  ///
+  ///allEndpoints
   static const String allEndpoints = 'allEndpoints';
 
-  ///
+  ///defaultEndpoint
   static const String defaultEndpoint = 'defaultEndpoint';
 
-  ///
+  ///defaultHMI
   static const String defaultHMI = 'defaultHMI';
 
-  ///
+  ///location
   static const String location = 'location';
 
-  ///
+  ///latitude
   static const String latitude = 'latitude';
 
-  ///
+  ///longitude
   static const String longitude = 'longitude';
 
-  ///
+  ///ownedBy
   static const String ownedBy = 'ownedBy';
 
-  ///
+  ///administratedBy
   static const String administratedBy = 'administratedBy';
 
-  ///
+  ///usedBy
   static const String usedBy = 'usedBy';
 
-  ///
+  ///represents
   static const String represents = 'represents';
 
-  ///
+  ///thingStructure
   static const String thingStructure = 'thingStructure';
 
-  ///
+  ///class
   static const String classString = 'class';
 
-  ///
+  ///links
   static const String links = 'links';
 
-  ///
+  ///identifier
   static const String identifier = 'identifier';
 
-  ///
+  ///values
   static const String values = 'values';
 
-  ///
+  ///association
   static const String association = 'association';
 
-  ///
+  ///target
   static const String target = 'target';
 
-  ///
+  ///attribute
   static const String attribute = 'attribute';
 
-  ///
+  ///value
   static const String value = 'value';
 }
