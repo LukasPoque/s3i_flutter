27c21ddd922cb376acf66ae2deed4c6ec1fd85f1
LukasPoque <lukas.poque@rwth-aachen.de>
2021-08-05 20:41:00 +0200

    Fix linter issues in json_key

--- lib/src/utils/json_key.dart
+++ lib/src/utils/json_key.dart
@@ -1,47 +1,127 @@
-/// Stores universal string keys
+/// Stores universal string keys constants.
 class JsonKey {
+  // TODO(poq): add documentation
 // ditto
-  static const String thingId = "thingId";
-  static const String policyId = "policyId";
-  static const String attributes = "attributes";
-  static const String items = "items";
-  static const String cursor = "cursor";
-  static const String type = "type";
-  static const String expiry = "expiry";
-  static const String grant = "grant";
-  static const String revoke = "revoke";
-  static const String subjects = "subjects";
-  static const String resources = "resources";
-  static const String entries = "entries";
+  ///
+  static const String thingId = 'thingId';
+
+  ///
+  static const String policyId = 'policyId';
+
+  ///
+  static const String attributes = 'attributes';
+
+  ///
+  static const String items = 'items';
+
+  ///
+  static const String cursor = 'cursor';
+
+  ///
+  static const String type = 'type';
+
+  ///
+  static const String expiry = 'expiry';
+
+  ///
+  static const String grant = 'grant';
+
+  ///
+  static const String revoke = 'revoke';
+
+  ///
+  static const String subjects = 'subjects';
+
+  ///
+  static const String resources = 'resources';
+
+  ///
+  static const String entries = 'entries';
 
 // keycloak
-  static const String grantType = "grant_type";
-  static const String clientId = "client_id";
-  static const String clientSecret = "client_secret";
-  static const String refreshToken = "refresh_token";
+  ///
+  static const String grantType = 'grant_type';
+
+  ///
+  ///
+  static const String clientId = 'client_id';
+
+  ///
+  static const String clientSecret = 'client_secret';
+
+  ///
+  static const String accessToken = 'access_token';
+
+  ///
+  static const String refreshToken = 'refresh_token';
 
 // (f)ml40 specific keys
-  static const String name = "name";
-  static const String thingType = "type";
-  static const String dataModel = "dataModel";
-  static const String publicKey = "publicKey";
-  static const String allEndpoints = "allEndpoints";
-  static const String defaultEndpoint = "defaultEndpoint";
-  static const String defaultHMI = "defaultHMI";
-  static const String location = "location";
-  static const String latitude = "latitude";
-  static const String longitude = "longitude";
-  static const String ownedBy = "ownedBy";
-  static const String administratedBy = "administratedBy";
-  static const String usedBy = "usedBy";
-  static const String represents = "represents";
-  static const String thingStructure = "thingStructure";
-  static const String classString = "class";
-  static const String links = "links";
-  static const String identifier = "identifier";
-  static const String values = "values";
-  static const String association = "association";
-  static const String target = "target";
-  static const String attribute = "attribute";
-  static const String value = "value";
+  ///
+  static const String name = 'name';
+
+  ///
+  static const String thingType = 'type';
+
+  ///
+  static const String dataModel = 'dataModel';
+
+  ///
+  static const String publicKey = 'publicKey';
+
+  ///
+  static const String allEndpoints = 'allEndpoints';
+
+  ///
+  static const String defaultEndpoint = 'defaultEndpoint';
+
+  ///
+  static const String defaultHMI = 'defaultHMI';
+
+  ///
+  static const String location = 'location';
+
+  ///
+  static const String latitude = 'latitude';
+
+  ///
+  static const String longitude = 'longitude';
+
+  ///
+  static const String ownedBy = 'ownedBy';
+
+  ///
+  static const String administratedBy = 'administratedBy';
+
+  ///
+  static const String usedBy = 'usedBy';
+
+  ///
+  static const String represents = 'represents';
+
+  ///
+  static const String thingStructure = 'thingStructure';
+
+  ///
+  static const String classString = 'class';
+
+  ///
+  static const String links = 'links';
+
+  ///
+  static const String identifier = 'identifier';
+
+  ///
+  static const String values = 'values';
+
+  ///
+  static const String association = 'association';
+
+  ///
+  static const String target = 'target';
+
+  ///
+  static const String attribute = 'attribute';
+
+  ///
+  static const String value = 'value';
 }
