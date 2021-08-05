e00d1c4476017612c75619a7603be66075c70d51
LukasPoque <lukas.poque@rwth-aachen.de>
2021-08-05 20:06:24 +0200

    Restructure client_identity and authentication_manager

--- lib/src/auth/client_identity.dart
+++ lib/src/auth/client_identity.dart
@@ -1,11 +1,21 @@
-class ClientIdentity {
-  final String id;
-  final String secret;
+import 'package:flutter/foundation.dart';
 
-  ClientIdentity(this.id, this.secret);
+/// Represents a simple `Client` in the OpenID-Connect terms for authentication
+/// at the S3I-IdentityProvider.
+@immutable
+class ClientIdentity {
+  /// Creates a [ClientIdentity] with the given [id] and the [secret] which
+  /// could be empty if the client is a public client.
+  const ClientIdentity(this.id, {this.secret = ''});
+
+  /// The id of the client, starts often with `s3i:` in the S3I.
+  final String id;
+
+  /// The secret of the client, is an empty string if the client is `public`.
+  final String secret;
 
   @override
   String toString() {
-    return "Client($id, $secret)";
+    return 'Client($id, $secret)';
   }
 }
