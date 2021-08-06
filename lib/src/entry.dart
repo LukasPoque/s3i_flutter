307d6fd45e9a09136f8bfe36ff8629fcc88c16d2
LukasPoque <lukas.poque@rwth-aachen.de>
2021-08-06 09:18:01 +0200

    Fix linting warnings in entry and json_serializable_object

--- lib/src/entry.dart
+++ lib/src/entry.dart
@@ -2,12 +2,16 @@ import 'package:s3i_flutter/src/json_serializable_object.dart';
 
 /// Represents a simple entry (thing or policy) in the Directory/Repository.
 abstract class Entry implements JsonSerializableObject {
-  final String id;
-
+  /// Creates an [Entry] with the given [id].
   Entry(this.id);
+
+  /// The unique identifier of this entry.
+  ///
+  /// In the S3I, most identifiers starts with `s3i:` following by an UUIDv4.
+  final String id;
 
   @override
   String toString() {
-    return "Entry($id)";
+    return 'Entry($id)';
   }
 }
--- lib/src/json_serializable_object.dart
+++ lib/src/json_serializable_object.dart
@@ -1,4 +1,6 @@
 /// Base class for all classes which could be represented in json
 abstract class JsonSerializableObject {
+  /// Returns the information stored in this object in a way the it could be
+  /// encoded via `jsonEncode()`.
   Map<String, dynamic> toJson();
 }
